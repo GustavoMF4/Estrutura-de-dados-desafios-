958dd8c254481d3cf7fb3c47c846b5ce0c847ace
Gustavo Menegon Fais Pistozzo <gustavo.mfais@gmail.com>
2026-06-07 16:05:40 -0300

    desafios 2

---
+++ desafio-aula-2/desafio1.c
@@ -0,0 +1,182 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+typedef struct no {
+    int info;
+    struct no * prox;
+} No;
+
+typedef No * Celula;
+
+typedef struct lista {
+    Celula inicio;
+    Celula fim;
+} Lista;
+
+typedef Lista * ListaLigada;
+
+
+
+
+ListaLigada novaLista() {
+    ListaLigada l = malloc(sizeof(Lista));
+    if (!l) return NULL;
+    l->inicio = NULL;
+    l->fim = NULL;
+    return l;
+}
+
+Celula novaCelula(int info) {
+    Celula celula = (Celula) malloc(sizeof(No));
+    celula->info = info;
+    celula->prox = NULL;
+
+    return celula;
+}
+
+void llPrint(ListaLigada lista) {
+    for (Celula aux = lista->inicio; aux != NULL; aux = aux->prox) {
+        printf("%d", aux->info);
+        if (aux->prox != NULL)
+            printf(" -> ");
+    }
+    printf("\n");
+}
+
+
+Celula llBusca(ListaLigada lista, int info) {
+    for (Celula aux = lista->inicio; aux != NULL; aux = aux->prox) {
+        if (aux->info == info) {
+            return aux;
+        }
+    }
+    return NULL;
+}
+
+Celula llGetPenultimo(ListaLigada lista) {
+    if (lista->inicio == NULL) {
+        return NULL;
+    }
+
+    Celula penultimo = NULL;
+    for(Celula aux = lista->inicio; aux->prox != NULL; aux = aux->prox) {
+        penultimo = aux;
+    }
+    return penultimo;
+}
+
+void llInsereNoInicio(ListaLigada lista, int info) {
+    Celula celula = novaCelula(info);
+    if (lista->inicio == NULL) { // lista é vazia
+        lista->inicio = celula;
+        lista->fim = celula;
+    }
+    else {
+        celula->prox = lista->inicio;
+        lista->inicio = celula;
+    }
+}
+
+void llInsereNoFim(ListaLigada lista, int info) {
+    Celula nova = novaCelula(info);
+    
+    if (lista->inicio == NULL) { 
+        lista->inicio = nova;
+        lista->fim = nova;
+        return;
+    }
+    lista->fim->prox = nova;
+    lista->fim = nova;
+}
+
+void llRemoveInicio(ListaLigada lista) {
+    if (lista->inicio == NULL) {
+        return;
+    }
+
+    Celula removido = lista->inicio;
+    lista->inicio = removido->prox;
+    free(removido); 
+}
+
+void llInsereDepoisDe(ListaLigada lista, int alvo, int info) {
+    Celula nova = novaCelula(info);
+    Celula buscada = llBusca(lista, alvo);
+    if (buscada != NULL) { 
+        nova->prox = buscada->prox;
+        buscada->prox = nova;
+    }
+    else {
+        llInsereNoFim(lista, info);
+        free(nova);
+    }
+}
+
+void llInsereAntesDe(ListaLigada lista, int alvo, int info) {
+    Celula nova = novaCelula(info);
+    Celula aux = NULL;
+    for (aux = lista->inicio; aux->prox != NULL && aux->prox->info != alvo; aux = aux->prox);
+    if (aux->prox != NULL) { 
+        nova->prox = aux->prox;
+        aux->prox = nova;
+    }
+    else {
+        llInsereNoInicio(lista, info);
+        free(nova);
+    }
+}
+
+
+Celula llRemove(ListaLigada lista, int alvo) {
+    Celula aux = NULL;
+    for (aux = lista->inicio; aux->prox != NULL && aux->prox->info != alvo; aux = aux->prox);
+    if (aux != NULL) { 
+        Celula removido = aux->prox;
+        aux->prox = aux->prox->prox;
+        free(removido);
+    }
+    
+    return NULL; 
+}
+
+
+void llRemoveFim(ListaLigada lista) {
+    Celula penultimo = llGetPenultimo(lista);
+    Celula removido = lista->fim;
+    lista->fim = penultimo;
+    penultimo->prox = NULL;
+    free(removido);
+}
+
+int somaLista(ListaLigada lista){
+    if (lista == NULL || lista -> inicio == NULL){
+        return 0;
+    }
+
+    Celula atual = lista->inicio;
+    int soma = 0;
+
+    while(atual != NULL){
+        soma += atual->info;
+        atual = atual ->prox;
+    }
+    return soma;
+}
+
+
+int main() {
+    ListaLigada lista = novaLista();
+
+    for (int i = 1; i <= 10; i++) {
+        llInsereNoFim(lista, i);
+    }
+
+    llPrint(lista);
+
+   int soma = somaLista(lista);
+    printf ("%d", soma);
+
+    return 0;
+}
---
+++ desafio-aula-2/desafio3.c
@@ -0,0 +1,203 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+typedef struct no {
+    int info;
+    struct no * prox;
+} No;
+
+typedef No * Celula;
+
+typedef struct lista {
+    Celula inicio;
+    Celula fim;
+} Lista;
+
+typedef Lista * ListaLigada;
+
+
+
+
+ListaLigada novaLista() {
+    ListaLigada l = malloc(sizeof(Lista));
+    if (!l) return NULL;
+    l->inicio = NULL;
+    l->fim = NULL;
+    return l;
+}
+
+Celula novaCelula(int info) {
+    Celula celula = (Celula) malloc(sizeof(No));
+    celula->info = info;
+    celula->prox = NULL;
+
+    return celula;
+}
+
+void llPrint(ListaLigada lista) {
+    for (Celula aux = lista->inicio; aux != NULL; aux = aux->prox) {
+        printf("%d", aux->info);
+        if (aux->prox != NULL)
+            printf(" -> ");
+    }
+    printf("\n");
+}
+
+
+Celula llBusca(ListaLigada lista, int info) {
+    for (Celula aux = lista->inicio; aux != NULL; aux = aux->prox) {
+        if (aux->info == info) {
+            return aux;
+        }
+    }
+    return NULL;
+}
+
+Celula llGetPenultimo(ListaLigada lista) {
+    if (lista->inicio == NULL) {
+        return NULL;
+    }
+
+    Celula penultimo = NULL;
+    for(Celula aux = lista->inicio; aux->prox != NULL; aux = aux->prox) {
+        penultimo = aux;
+    }
+    return penultimo;
+}
+
+void llInsereNoInicio(ListaLigada lista, int info) {
+    Celula celula = novaCelula(info);
+    if (lista->inicio == NULL) { // lista é vazia
+        lista->inicio = celula;
+        lista->fim = celula;
+    }
+    else {
+        celula->prox = lista->inicio;
+        lista->inicio = celula;
+    }
+}
+
+void llInsereNoFim(ListaLigada lista, int info) {
+    Celula nova = novaCelula(info);
+    
+    if (lista->inicio == NULL) { 
+        lista->inicio = nova;
+        lista->fim = nova;
+        return;
+    }
+    lista->fim->prox = nova;
+    lista->fim = nova;
+}
+
+void llRemoveInicio(ListaLigada lista) {
+    if (lista->inicio == NULL) {
+        return;
+    }
+
+    Celula removido = lista->inicio;
+    lista->inicio = removido->prox;
+    free(removido); 
+}
+
+void llInsereDepoisDe(ListaLigada lista, int alvo, int info) {
+    Celula nova = novaCelula(info);
+    Celula buscada = llBusca(lista, alvo);
+    if (buscada != NULL) { 
+        nova->prox = buscada->prox;
+        buscada->prox = nova;
+    }
+    else {
+        llInsereNoFim(lista, info);
+        free(nova);
+    }
+}
+
+void llInsereAntesDe(ListaLigada lista, int alvo, int info) {
+    Celula nova = novaCelula(info);
+    Celula aux = NULL;
+    for (aux = lista->inicio; aux->prox != NULL && aux->prox->info != alvo; aux = aux->prox);
+    if (aux->prox != NULL) { 
+        nova->prox = aux->prox;
+        aux->prox = nova;
+    }
+    else {
+        llInsereNoInicio(lista, info);
+        free(nova);
+    }
+}
+
+
+Celula llRemove(ListaLigada lista, int alvo) {
+    Celula aux = NULL;
+    for (aux = lista->inicio; aux->prox != NULL && aux->prox->info != alvo; aux = aux->prox);
+    if (aux != NULL) { 
+        Celula removido = aux->prox;
+        aux->prox = aux->prox->prox;
+        free(removido);
+    }
+    
+    return NULL; 
+}
+
+
+void llRemoveFim(ListaLigada lista) {
+    Celula penultimo = llGetPenultimo(lista);
+    Celula removido = lista->fim;
+    lista->fim = penultimo;
+    penultimo->prox = NULL;
+    free(removido);
+}
+
+void insereOrdenado(ListaLigada lista, int valor){
+    if (lista == NULL) return;
+
+    Celula novo = novaCelula(valor);
+    if (novo == NULL) return;
+
+    
+    if (lista->inicio == NULL){
+        lista->inicio = novo;
+        lista->fim = novo;
+        return;
+    }
+
+    Celula atual = lista->inicio;
+    Celula anterior = NULL;
+
+    
+    while(atual != NULL && atual->info < valor){
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (anterior == NULL){
+        novo->prox = lista->inicio;
+        lista->inicio = novo;
+    }
+    else{
+        anterior->prox = novo;
+        novo->prox = atual;
+    }
+
+    
+    if (novo->prox == NULL){
+        lista->fim = novo;
+    }
+}
+
+
+
+int main() {
+    ListaLigada lista = novaLista();
+
+    for (int i = 20; i >= 0; i--) {
+        insereOrdenado(lista, i);
+    }
+
+    llPrint(lista);
+
+
+    return 0;
+}
